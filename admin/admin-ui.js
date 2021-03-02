ca11c8ff703b32de8f166b3e2335f1c589dfb2f8
vasyaivanov <vasyaivanov@users.noreply.github.com>
2021-03-01 18:12:54 -0800

    open admin links in new tab

--- admin/admin-ui.js
+++ admin/admin-ui.js
@@ -182,7 +182,7 @@ function updateListOfRooms(rooms) {
         html += '<td>' + (idx + 1) + '</td>';
         html += '<td><span class="max-width" title="' + roomid + '">' 
             + '<a href="' + window.location.protocol + '//' + window.location.hostname 
-            + '/demos/audio-conferencing.html?roomid=' + roomid + '">' + roomid + '</a></span></td>';
+            + '/demos/audio-conferencing.html?roomid=' + roomid + '" target="_blank">' + roomid + '</a></span></td>';
         html += '<td><span class="max-width" title="' + room.owner + '">' + room.owner + '</span></td>';
         html += '<td><span class="max-width" title="' + room.identifier + '">' + (room.identifier || 'None') + '</span></td>';
         html += '<td><span class="max-width" title="' + room.socketMessageEvent + '">' + room.socketMessageEvent + '</span></td>';
