d409996ebe0ae7397578690cb86c81cbf062f35c
vasyaivanov <vasyaivanov@users.noreply.github.com>
2021-03-03 22:56:03 -0800

    Trying send message from server to screenshare2.html again

--- server.js
+++ server.js
@@ -288,6 +288,8 @@ ioServer(httpApp).on('connection', function(socket) {
 
     socket.on('open-screenshare', function(message){
         console.log("JD: RECEIVED MESSAGE open-screenshare:"+JSON.stringify(message)+" "+message.socketid);
+        console.log("JD: socket from sockeid="+ioServer.sockets.connected[message.socketid]);
+        // socket.broadcast.emit('open-sreenshare-in-room',message.socketid);
     });
 
     socket.on(params.socketCustomEvent, function(message) {
